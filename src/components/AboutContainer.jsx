f28b662a5ad30be230822dcfd0cd3cd1b16614db
tomazss <80653661+tomazss@users.noreply.github.com>
2023-05-18 13:32:16 -0300

    Update AboutContainer.jsx

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,8 +4,25 @@ const AboutContainer = (props) => {
     return (
         <section className="about-container">
             <h2>Sobre</h2>
-                <p>Estudante de Ciência da Computação no Centro Universitário Unifacvest, buscando novos desafios como desenvolvedor. 
-                </p>
+               <p>Olá,</p>
+      <p>
+        Sou <strong>Tomaz Silva Sampaio</strong>,
+        estudante de Ciência da Computação
+        apaixonado por tecnologia
+        e inovação. Meu portfólio é um reflexo do meu compromisso em buscar
+        conhecimento e aplicar minhas habilidades em projetos práticos. Nele,
+        você encontrará exemplos dos meus trabalhos em 
+        programação e soluções criativas.
+      </p>
+      <p> Estou constantemente buscando novos desafios e oportunidades para expandir
+      minha expertise. Minhas experiências acadêmicas e projetos pessoais
+      demonstram minha dedicação em aprender novas tecnologias e encontrar
+      soluções eficientes para problemas complexos.</p>
+
+      <p>Seja bem-vindo(a) ao meu
+      portfólio. Espero que você aprecie meu trabalho e veja o potencial que
+      posso trazer para futuras colaborações e oportunidades no campo</p>
+      
                 
         </section>
     );
